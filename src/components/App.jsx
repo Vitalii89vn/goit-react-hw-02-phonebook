7cb91fc71bd8d9bf9d1d1d622af751613181f5ae
Vitalii89vn <vitalii.kuhta@gmail.com>
2023-01-31 23:35:50 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
-export const App = () => {
+import { Component } from "react";
+import { ContactForm } from "./ContactForm";
+
+export class App extends Component {
+  state = {
+  contacts: [],
+  name: '',
+  number: ''
+}
+  
+  
+  render() {
   return (
     <div>
-      Hello
+      <h1>Phonebook</h1>
+        <ContactForm />
+      <h2>Contacts</h2>
+
     </div>
-  );
+  )
+}
 };
